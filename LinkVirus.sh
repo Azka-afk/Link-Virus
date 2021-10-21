2d6cdae2dd316e886ad331b6041e368eb30087af
Azka <91669872+Azka-afk@users.noreply.github.com>
2021-10-21 19:07:39 +0700

    Add files via upload

---
+++ LinkVirus.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+#copyright MR.42K4
+
+#variabel
+e=echo
+s=sleep
+v=https
+z=bit.ly
+k=3ild93L
+
+
+#pembersih
+c=clear
+$c #pembersih
+
+#tampilan
+$s 2
+figlet Link-Virus | lolcat
+$s 1
+$e "==================================================================================" | lolcat
+$e " {+} Author : MR.42K4 " | lolcat
+$e " {+} Team  : Garuda Sniper Team" | lolcat
+$e "=================================================================================" | lolcat
+$s 1
+#daftar menu
+echo "                      <•>Welcome To User Termux<•>              " | lolcat
+echo "=================================================================================" | lolcat
+echo "{1}.Link Virus" | lolcat
+echo "{×}.Keluar" | lolcat
+echo "=================================================================================" | lolcat
+read -p "{×}Pilih : " option;
+#data
+if [[ $option == "1" ]]
+then
+	echo
+	echo "Sabar......" | lolcat
+	sleep 4
+	$e "÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷" | lolcat
+	$e "   		           ↓↓↓↓↓↓↓↓"
+	$e
+	$e "                    $v://$z/$k  "
+	echo "÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷" | lolcat
+else
+	echo "babay...."
+fi
